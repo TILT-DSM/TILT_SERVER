385162de37ed4eec6f3b274e18a04518eff0c0e7
imhanseong <hans.hanseong@gmail.com>
2025-10-22 15:35:50 +0900

    fix :: GoldFacade 수정

--- src/main/kotlin/org/example/tilt_server/gold/facade/GoldFacade.kt
+++ src/main/kotlin/org/example/tilt_server/gold/facade/GoldFacade.kt
@@ -1,12 +1,22 @@
 package org.example.tilt_server.gold.facade
 
 import org.example.tilt_server.gold.service.GetGoldPriceService
+import org.example.tilt_server.gold.service.GoogleNewsService
 import org.springframework.stereotype.Component
 
 @Component
 class GoldFacade(
-    private val getGoldPriceService: GetGoldPriceService
-
+    private val getGoldPriceService: GetGoldPriceService,
+    private val googleNewsService: GoogleNewsService
 ) {
-    fun getGoldInfo() = getGoldPriceService.execute()
+    fun getGoldInfo(): Map<String, Any> {
+        val result = getGoldPriceService.execute()
+        val news = googleNewsService.getGoldNews()
+
+        return mapOf(
+            "data" to result.data,
+            "analysis" to result.analysis,
+            "news" to news
+        )
+    }
 }
