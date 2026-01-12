c3d9db03826fb1cc91fb6610f5fcb69eec1f7727
imhanseong <hans.hanseong@gmail.com>
2026-01-12 19:25:43 +0900

    feat :: Cors 추가

---
+++ src/main/kotlin/org/example/tiltserver/global/config/CorsConfig.kt
@@ -0,0 +1,21 @@
+package org.example.tiltserver.global.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsWebFilter
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("https://*.xquare.*", "http://localhost:*")
+            .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600)
+    }
+}
