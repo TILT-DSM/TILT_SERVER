874248bfa997598e4c0a80e666876a33779b809e
imhanseong <hans.hanseong@gmail.com>
2026-01-15 09:48:12 +0900

    fix :: entity 변경

--- src/main/kotlin/org/example/tiltserver/gold/entity/GoldPrice.kt
+++ src/main/kotlin/org/example/tiltserver/gold/entity/GoldPrice.kt
@@ -3,8 +3,5 @@ package org.example.tiltserver.gold.entity
 data class GoldPrice(
     val price: Double,
     val currency: String,
-    val timestamp: Long,
-    val open: Double?,
-    val high: Double?,
-    val low: Double?
+    val timestamp: Long
 )
