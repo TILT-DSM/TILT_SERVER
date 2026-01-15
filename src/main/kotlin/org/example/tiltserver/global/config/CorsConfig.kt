0318f8f8ba755c6bcb80e685f14b9d735a1539e7
imhanseong <hans.hanseong@gmail.com>
2026-01-15 09:48:33 +0900

    fix :: 공백 수정

--- src/main/kotlin/org/example/tiltserver/global/config/CorsConfig.kt
+++ src/main/kotlin/org/example/tiltserver/global/config/CorsConfig.kt
@@ -1,10 +1,6 @@
 package org.example.tiltserver.global.config
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.reactive.CorsWebFilter
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -12,8 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class CorsConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOriginPatterns("https://*.xquare.*", "http://localhost:*","https://*.vercel.app")
-            .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
+            .allowedOriginPatterns("https://*.xquare.*", "http://localhost:*", "https://*.vercel.app")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
             .maxAge(3600)
