217c01fcac137d920e05c879988731bff1c47fcb
imhanseong <hans.hanseong@gmail.com>
2026-01-15 09:47:30 +0900

    fix :: DTO 수정

--- src/main/kotlin/org/example/tiltserver/gold/persistence/dto/GoldApiRawResponse.kt
+++ src/main/kotlin/org/example/tiltserver/gold/persistence/dto/GoldApiRawResponse.kt
@@ -1,10 +1,11 @@
 package org.example.tiltserver.gold.persistence.dto
 
 data class GoldApiRawResponse(
-    val price: Double?,
-    val currency: String?,
+    val base: String?,
     val timestamp: Long?,
-    val open: Double?,
-    val high: Double?,
-    val low: Double?
-)
+    val rates: Rates?
+) {
+    data class Rates(
+        val XAU: Double?
+    )
+}
