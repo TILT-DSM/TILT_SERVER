1d855964b037464c252372b56e0effc86fc637ef
imhanseong <hans.hanseong@gmail.com>
2026-01-15 09:47:18 +0900

    fix :: url 변경

--- src/main/kotlin/org/example/tiltserver/gold/presentation/controller/GoldController.kt
+++ src/main/kotlin/org/example/tiltserver/gold/presentation/controller/GoldController.kt
@@ -1,6 +1,7 @@
 package org.example.tiltserver.gold.presentation.controller
 
-import org.example.tiltserver.gold.facade.GoldFacade
+import org.example.tiltserver.gold.entity.GoldPrice
+import org.example.tiltserver.gold.service.GetGoldPriceService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -8,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/gold")
 class GoldController(
-    private val goldFacade: GoldFacade
+    private val goldService: GetGoldPriceService
 ) {
     @GetMapping("/price")
-    fun getGold() = goldFacade.getGoldInfo()
+    fun getGold(): GoldPrice = goldService.execute().goldPrice
 }
