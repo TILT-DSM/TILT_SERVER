4a57ac76e84018d0f36a2ee04133e4200b38eb18
imhanseong <hans.hanseong@gmail.com>
2026-01-21 16:02:51 +0900

    fix :: Response 수정

--- src/main/kotlin/org/example/tiltserver/gold/persistence/dto/GoldApiRawResponse.kt
+++ src/main/kotlin/org/example/tiltserver/gold/persistence/dto/GoldApiRawResponse.kt
@@ -1,6 +1,10 @@
 package org.example.tiltserver.gold.persistence.dto
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class GoldApiRawResponse(
+    val success: Boolean?,
     val base: String?,
     val timestamp: Long?,
     val rates: Rates?
